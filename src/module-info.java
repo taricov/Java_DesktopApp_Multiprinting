b7063cbfe7eae2a69faf6a404935b84d740d8a4d
TaricOv <tareq.ouf@gmail.com>
2024-10-29 13:34:31 +0300

    initial commit . just created a new javaapp for making the multiprinting desktop app

---
+++ src/module-info.java
@@ -0,0 +1,16 @@
+module desktop.app.core {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens desktop.app.core to javafx.fxml;
+    exports desktop.app.core;
+}
